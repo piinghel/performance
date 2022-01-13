af557aa842e6c17ea6d0a9b21efead90276b32ba
piinghel <pjinghelbrecht>
2022-01-13 18:45:28 +0100

    updated project structure

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from struct import pack
 from setuptools import setup, find_packages
 
 setup(
-    name="performance_analysis",
+    name="performance_evaluation",
     version="0.0.1",
-    packages=find_packages("performance_analysis"),
+    packages=find_packages("performance_evaluation"),
     install_requires=["pandas>=1.3.4" "scipy>=1.7.2", "numpy>=1.19.2"],
 )
