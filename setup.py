fad9515114866ecc378043ea2d4a75c644446f78
piinghel <pjinghelbrecht>
2022-01-14 13:08:39 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="performance_evaluation",
-    version="0.0.3",
+    version="0.1.0",
     packages=find_packages("performance_evaluation"),
     install_requires=["pandas>=1.3.4" "scipy>=1.7.2", "numpy>=1.19.2"],
 )
